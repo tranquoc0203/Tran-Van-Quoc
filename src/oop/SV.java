81f3ba1677191b5671b2540d3ff8bbb118e397ed
TranVanQuoc <tranquoc0302@gmal.com>
2019-10-22 16:44:34 +0700

    BT 4

--- src/oop/SV.java
+++ src/oop/SV.java
@@ -1,10 +1,103 @@
 package oop;
 
+import java.util.Date;
+
 public class SV {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+	
+    private String hoTen;
+    private Date ngaySinh;
+    private double dtb;
+    
+    public SV (String hoTen,Date ngaySinh,double dtb){
+        this.hoTen=hoTen;
+        this.ngaySinh=ngaySinh;
+        this.dtb=dtb;
+    }
+    
+    public String layHoTen(){
+        return hoTen;
+    }
+    
+    public void ganHoTen(String ht){
+        hoTen=ht;
+    }
+    
+    public Date layNgaySinh(){
+        return ngaySinh;
+    }
+    
+    public void ganNgaySinh(Date ns){
+        ngaySinh=ns;
+    }
+    
+     public double laydtb(){
+        return dtb;
+    }
+     
+    public void gandtb(double dtb){
+        this.dtb=dtb;
+    }
+    
+    public void hienThi(){
+    	
+        System.out.println("ten la:"+hoTen+"\nngay sinh la: "+ngaySinh
+                +"\ndtb la: "+dtb+"\nlay tuoi: "
+                +this.laytuoi()+"\nho: "+this.layHo()+"\nten: "
+                +this.layTen()+"\nho dem: "
+                +this.layhodem()+"\ntuoi: "+this.laytuoi()+"\nxep loai: "+this.xepLoai());
+    }
+    
+    public String layHo(){
+        String ho;
+        int i= hoTen.indexOf(' ');
+        ho =hoTen.substring(0,i);
+        return ho;
+    }
+    
+    public String layTen(){
+        String Ten;
+       
+        int i= hoTen.lastIndexOf(' ');
+        Ten =hoTen.substring(i+1);
+        return Ten;
+    }
+   
+    public String layhodem(){
+        String hoDem;
+        hoDem =hoTen.substring(hoTen.indexOf(' '),hoTen.lastIndexOf(' '));
+        return hoDem;
+    }
+    
+    public int laytuoi(){
+        int yearsNow;
+        int bd;
+        yearsNow=new Date().getYear()+1900;
+        bd= ngaySinh.getYear()+1900;
+        return yearsNow-bd;
+    }
+    
+    public String xepLoai(){
+        String xepLoai ="";
+        if(dtb<5){
+            xepLoai= "yeu";
+        }    
+        else if(dtb<7){
+            xepLoai= "Trung binh";
+        }
+        else if(dtb<8){
+            xepLoai= "kha";
+        }
+        else{
+            xepLoai= "gioi";
+        }
+        return xepLoai;
+    }  
+       
+   
+    public static void main(String[] args) {
+       SV p = new SV("Tran Van Quoc",new Date("02/03/2000"),10);
+       p.hienThi();
+    }
+   
 }
+
